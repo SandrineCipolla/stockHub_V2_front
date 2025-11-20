b1df03aa80ef0dca1755104716d59c49b53c3086
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2025-11-20 13:52:19 +0100

    fix: update vite.config.ts for ESM compatibility and define __dirname

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     const base = env.VITE_BASE_PATH || '/';
@@ -9,7 +11,7 @@ export default defineConfig(({ mode }) => {
         plugins: [react()],
         resolve: {
             alias: {
-                '@': path.resolve(process.cwd(), 'src'),
+                '@': path.resolve(__dirname, 'src'),
             },
         },
         build: {
